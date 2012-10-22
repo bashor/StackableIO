f03803bb9d38ec5cdea555acc1481c3105b15509
Zalim Bashorov <bashor@ya.ru>
2012-10-22 23:35:19 +0400

    init tests

---
+++ tests/Test.scala
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class Test extends FunSuite {
+  test("test"){}
+
+}
